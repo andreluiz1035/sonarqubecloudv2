2531a0877b46415dea9c3c7b1432e844133c8122
andreluiz1035 <andreluiz.mcse@gmail.com>
2025-11-26 13:41:10 -0300

    10

---
+++ hello_coverage/tests/test_hello.py
@@ -0,0 +1,8 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from hello import say_hello
+
+def test_say_hello():
+    assert say_hello("world") == "Hello, world!"
