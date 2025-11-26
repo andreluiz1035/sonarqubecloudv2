2757c7c97fb249cc49b7550d20575312f9a9d443
andreluiz1035 <andreluiz.mcse@gmail.com>
2025-11-26 18:06:59 -0300

    31

---
+++ hello_coverage/code_smell_dead_code.py
@@ -0,0 +1,7 @@
+# Função que nunca é usada → Code Smell clássico
+def funcao_inutil():
+    x = 10  # variável nunca usada
+    return x
+
+def soma(a, b):
+    return a + b
---
+++ hello_coverage/tests/test_code_smell_dead_code.py
@@ -0,0 +1,4 @@
+from code_smell_dead_code import soma
+
+def test_soma():
+    assert soma(2, 3) == 5
