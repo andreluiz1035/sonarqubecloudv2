89799ddece6ffe28ceb59d655b0aae7b1e360c9c
andreluiz1035 <andreluiz.mcse@gmail.com>
2025-12-09 13:07:46 -0300

    comentario duplicated 55

--- hello_coverage/duplicado.py
+++ hello_coverage/duplicado.py
@@ -5,3 +5,10 @@ def soma(a, b):
 # Função 2 — duplicada
 def soma_numeros(x, y):
     return x + y
+
+def soma(a, b):
+    return a + b
+
+# Função 2 — duplicada
+def soma_numeros(x, y):
+    return x + y
