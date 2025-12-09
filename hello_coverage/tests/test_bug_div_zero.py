88d5ff108ecef07530ecc8f8b72d2b23c8c1c258
andreluiz1035 <andreluiz.mcse@gmail.com>
2025-12-09 12:10:02 -0300

    comentario print 51

--- hello_coverage/tests/test_bug_div_zero.py
+++ hello_coverage/tests/test_bug_div_zero.py
@@ -1,5 +1,20 @@
 import pytest
 
 def test_bug_div_zero():
-    with pytest.raises(ZeroDivisionError):
+ #   with pytest.raises(ZeroDivisionError):
+ #       import bug_div_zero
+    try:
+
         import bug_div_zero
+
+    except ZeroDivisionError:
+
+        assert False   # só entra aqui se ocorrer erro
+
+    else:
+
+        assert True  # se não deu erro, teste falha
+
+
+        
+ 
