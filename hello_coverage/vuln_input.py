06d99f922ee5e836149a2bd9db8bc421fba78e18
andreluiz1035 <andreluiz.mcse@gmail.com>
2026-03-01 14:27:25 -0300

    correção hardcoded pass

--- hello_coverage/vuln_input.py
+++ hello_coverage/vuln_input.py
@@ -1,12 +1,12 @@
 # Vulnerabilidade REAL: credenciais hardcoded
-USERNAME = "admin"
-PASSWORD = "123456"  # Sonar marca isso como Vulnerability
+#USERNAME = "admin"
+#PASSWORD = "123456"  # Sonar marca isso como Vulnerability
 
 def autenticar(usuario, senha):
     return usuario == USERNAME and senha == PASSWORD
 
 ######teste
 
-#username = os.getenv("username") # Compliant
-#password = os.getenv("password") # Compliant
-#usernamePassword = 'user=%s&password=%s' % (username, password) # Compliant
+username = os.getenv("username") # Compliant
+password = os.getenv("password") # Compliant
+usernamePassword = 'user=%s&password=%s' % (username, password) # Compliant
